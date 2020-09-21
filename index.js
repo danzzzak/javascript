2fabcfe809e0cfbe81013cdbf130467985456540
danzzzak <siriuz2000@mail.ru>
2020-09-21 17:33:09 +0500

    test1

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-"use strict";
+"use strict"
 
-
-console.log(1)
+const numberOfFilms = +prompt('Skolko filmov posmotreli', '');
+let personalMovieDB = {
+    count: +numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+let lastFilm1 = prompt('kakoi posledniy film posmotreli?', '');
+let ocencaLastFilm1 = prompt('kakaya ego ocenka?', '');
+let lastFilm2 = prompt('kakoi posledniy film posmotreli?', '');
+let ocencaLastFilm2 = prompt('kakaya ego ocenka?', '');
+personalMovieDB.movies[lastFilm1] = ocencaLastFilm1;
+personalMovieDB.movies[lastFilm2] = ocencaLastFilm2;
+console.log(personalMovieDB);
