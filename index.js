cb6c37df8e5467f442bd67ded60ddb5a4ae39e50
danzzzak <siriuz2000@mail.ru>
2020-09-21 21:08:51 +0500

    test2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict"
-
+let countSwitch;
 const numberOfFilms = +prompt('Skolko filmov posmotreli', '');
 let personalMovieDB = {
     count: +numberOfFilms,
@@ -8,10 +8,25 @@ let personalMovieDB = {
     genres: [],
     privat: false,
 };
-let lastFilm1 = prompt('kakoi posledniy film posmotreli?', '');
-let ocencaLastFilm1 = prompt('kakaya ego ocenka?', '');
-let lastFilm2 = prompt('kakoi posledniy film posmotreli?', '');
-let ocencaLastFilm2 = prompt('kakaya ego ocenka?', '');
-personalMovieDB.movies[lastFilm1] = ocencaLastFilm1;
-personalMovieDB.movies[lastFilm2] = ocencaLastFilm2;
+
+for (let i = 0; i < 2; i++) {
+    let lastFilm1 = prompt('kakoi posledniy film posmotreli?', '');
+    let ocencaLastFilm1 = prompt('kakaya ego ocenka?', '');
+    if (lastFilm1 != null && ocencaLastFilm1 != null && lastFilm1 != '' && ocencaLastFilm1 != '' && lastFilm1.length < 50) {
+        personalMovieDB.movies[lastFilm1] = ocencaLastFilm1;
+    } else {
+        i--;
+        console.log('promt error');
+    }
+}
+if (personalMovieDB.count < 10) {
+    console.log('malo filmov');
+} else if (personalMovieDB.count <= 30) {
+    console.log('klassike');
+} else if (30 < personalMovieDB.count) {
+    console.log('kinoman');
+} else {
+    console.log('oshibka');
+}
+
 console.log(personalMovieDB);
